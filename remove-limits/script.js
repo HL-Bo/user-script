548ec0297f36c7f62ca7c5be02749391072a1f83
HL-Bo <hl-bo@outlook.com>
2024-05-05 19:34:34 +0800

    Update to version 2.2

--- remove-limits/script.js
+++ remove-limits/script.js
@@ -5,14 +5,17 @@
 // @namespace            http://hl-bo.github.io/namespaces/user-script/remove-limits
 // @source               https://github.com/HL-Bo/user-script
 // @supportURL           https://github.com/HL-Bo/user-script/issues
-// @version              2.1
+// @version              2.2
 // @license              AGPLv3
 // @description          Allows you select, cut, copy, paste, save and open the DevTools on any website.
 // @description:zh-CN    恢复选择、剪切、复制、粘贴、保存、右键菜单和打开开发者工具的默认行为。
 // @description:en-US    Allows you select, cut, copy, paste, save and open the DevTools on any website.
 // @author               HL-Bo
 // @match                *://*/**
+// @exclude              *://vscode.dev
 // @exclude              *://vscode.dev/**
+// @exclude              *://*.github.dev
+// @exclude              *://*.github.dev/**
 // @exclude              *://github.com/*/*/edit/**
 // @exclude              *://gitee.com/*/*/edit/**
 // @exclude              *://codeberg.org/*/*/_edit/**
@@ -50,6 +53,10 @@
         }
     } catch (error) { } finally { }
 
+    let executeWithInterval = function (func, delay) {
+        setTimeout(func, 0); // 异步执行，防止阻塞
+        setInterval(func, delay);
+    };
     let setEventListener = function (element, event_name, listener) {
         if (element.rl_events) {
             if (element.rl_events.has(event_name)) {
@@ -60,7 +67,7 @@
         }
         element.rl_events.set(event_name, listener);
         element.addEventListener(event_name, listener);
-    }
+    };
     let returnEventAllowed = function (event, event_name) {
         try {
             event.returnValue = true;
@@ -90,34 +97,124 @@
     let allowKeyEvents = function (element, event_name) {
         setEventListener(element, event_name, onKeyEvents);
     };
+    let preventEventChecks = function (element) {
+        Object.defineProperty(element, 'onbeforecopy', { get: () => (event) => false });
+        Object.defineProperty(element, 'oncopy', { get: () => (event) => false });
+        Object.defineProperty(element, 'onbeforecut', { get: () => (event) => false });
+        Object.defineProperty(element, 'oncut', { get: () => (event) => false });
+        Object.defineProperty(element, 'onbeforepaste', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpaste', { get: () => (event) => false });
+        Object.defineProperty(element, 'onselectstart', { get: () => (event) => false });
+        Object.defineProperty(element, 'oncontextmenu', { get: () => (event) => false });
+        Object.defineProperty(element, 'ondragstart', { get: () => (event) => false });
+        Object.defineProperty(element, 'ondragenter', { get: () => (event) => false });
+        Object.defineProperty(element, 'ondragover', { get: () => (event) => false });
+        Object.defineProperty(element, 'ondragleave', { get: () => (event) => false });
+        Object.defineProperty(element, 'ondragend', { get: () => (event) => false });
+        Object.defineProperty(element, 'ondrop', { get: () => (event) => false });
+        Object.defineProperty(element, 'onkeypress', { get: () => (event) => false });
+        Object.defineProperty(element, 'onkeydown', { get: () => (event) => false });
+        Object.defineProperty(element, 'onkeyup', { get: () => (event) => false });
+        Object.defineProperty(element, 'onvisibilitychange', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmousedown', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmouseup', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmousewheel', { get: () => (event) => false });
+        Object.defineProperty(element, 'onwheel', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmouseenter', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmousemove', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmouseover', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmouseout', { get: () => (event) => false });
+        Object.defineProperty(element, 'onmouseleave', { get: () => (event) => false });
+        Object.defineProperty(element, 'ongotpointercapture', { get: () => (event) => false });
+        Object.defineProperty(element, 'onlostpointercapture', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerdown', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerrawupdate', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerup', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerenter', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointermove', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerover', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerout', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointerleave', { get: () => (event) => false });
+        Object.defineProperty(element, 'onpointercancel', { get: () => (event) => false });
+        Object.defineProperty(element, 'onfocus', { get: () => (event) => false });
+        Object.defineProperty(element, 'onfocusin', { get: () => (event) => false });
+        Object.defineProperty(element, 'onfocusout', { get: () => (event) => false });
+        Object.defineProperty(element, 'onblur', { get: () => (event) => false });
+    };
     let allowElement = function (element) {
+        // 取消通过 JavaScript 实现的禁止复制
+        try { element.onbeforecopy = null; } catch (error) { } finally { allowEvent(element, 'beforecopy'); }
         try { element.oncopy = null; } catch (error) { } finally { allowEvent(element, 'copy'); }
+        // 取消通过 JavaScript 实现的禁止剪切实现的禁止复制
+        try { element.onbeforecut = null; } catch (error) { } finally { allowEvent(element, 'beforecut'); }
+        try { element.oncut = null; } catch (error) { } finally { allowEvent(element, 'cut'); }
+        // 取消通过 JavaScript 实现的禁止粘贴
+        try { element.onbeforepaste = null; } catch (error) { } finally { allowEvent(element, 'beforepaste'); }
+        try { element.onpaste = null; } catch (error) { } finally { allowEvent(element, 'paste'); }
         // 取消通过 JavaScript 实现的禁止文字选择
         try { element.onselectstart = null; } catch (error) { } finally { allowEvent(element, 'selectstart'); }
         // 取消通过 JavaScript 实现的禁止右键菜单
         try { element.oncontextmenu = null; } catch (error) { } finally { allowEvent(element, 'contextmenu'); }
-        // 取消通过 JavaScript 实现的禁止剪切实现的禁止复制
-        try { element.oncut = null; } catch (error) { } finally { allowEvent(element, 'cut'); }
-        // 取消通过 JavaScript 实现的禁止粘贴
-        try { element.onpaste = null; } catch (error) { } finally { allowEvent(element, 'paste'); }
+        // 取消通过 JavaScript 实现的禁止拖动
+        try { element.ondragstart = null; } catch (error) { } finally { allowEvent(element, 'dragstart'); }
+        try { element.ondragenter = null; } catch (error) { } finally { allowEvent(element, 'dragenter'); }
+        try { element.ondragover = null; } catch (error) { } finally { allowEvent(element, 'dragover'); }
+        try { element.ondragleave = null; } catch (error) { } finally { allowEvent(element, 'dragleave'); }
+        try { element.ondragend = null; } catch (error) { } finally { allowEvent(element, 'dragend'); }
+        // try { element.ondrop = null; } catch (error) { } finally { allowEvent(element, 'drop'); }
         // 取消通过 CSS 实现的禁止选中
-        try { element.style.webkitUserSelect = 'auto'; } catch (error) { } finally { } // Firefox
-        try { element.style.userSelect = 'auto'; } catch (error) { } finally { } // Chrome
+        try { element.style.mozUserSelect = 'auto'; } catch (error) { } finally { }
+        try { element.style.webkitUserSelect = 'auto'; } catch (error) { } finally { }
+        try { element.style.msUserSelect = 'auto'; } catch (error) { } finally { }
+        try { element.style.userSelect = 'auto'; } catch (error) { } finally { }
         // 取消通过 JavaScript 实现的禁用快捷键
         try { element.onkeypress = null; } catch (error) { } finally { allowKeyEvents(element, 'keypress'); }
         try { element.onkeydown = null; } catch (error) { } finally { allowKeyEvents(element, 'keydown'); }
         try { element.onkeyup = null; } catch (error) { } finally { allowKeyEvents(element, 'keyup'); }
         // 取消通过 JavaScript 实现的页面离开检测
         try { element.onvisibilitychange = null; } catch (error) { } finally { allowEvent(element, 'visibilitychange'); }
+        // 取消通过 JavaScript 实现的鼠标离开检测
+        // try { element.onmousedown = null; } catch (error) { } finally { allowEvent(element, 'mousedown'); }
+        // try { element.onmouseup = null; } catch (error) { } finally { allowEvent(element, 'mouseup'); }
+        // try { element.onmousewheel = null; } catch (error) { } finally { allowEvent(element, 'mousewheel'); }
+        // try { element.onwheel = null; } catch (error) { } finally { allowEvent(element, 'wheel'); }
+        // try { element.onmouseenter = null; } catch (error) { } finally { allowEvent(element, 'mouseenter'); }
+        // try { element.onmousemove = null; } catch (error) { } finally { allowEvent(element, 'mousemove'); }
+        // try { element.onmouseover = null; } catch (error) { } finally { allowEvent(element, 'mouseover'); }
         try { element.onmouseout = null; } catch (error) { } finally { allowEvent(element, 'mouseout'); }
         try { element.onmouseleave = null; } catch (error) { } finally { allowEvent(element, 'mouseleave'); }
-    }
+        // 取消通过 JavaScript 实现的指针离开检测
+        // try { element.ongotpointercapture = null; } catch (error) { } finally { allowEvent(element, 'gotpointercapture'); }
+        try { element.onlostpointercapture = null; } catch (error) { } finally { allowEvent(element, 'lostpointercapture'); }
+        // try { element.onpointerdown = null; } catch (error) { } finally { allowEvent(element, 'pointerdown'); }
+        // try { element.onpointerrawupdate = null; } catch (error) { } finally { allowEvent(element, 'pointerrawupdate'); }
+        try { element.onpointerup = null; } catch (error) { } finally { allowEvent(element, 'pointerup'); }
+        // try { element.onpointerenter = null; } catch (error) { } finally { allowEvent(element, 'pointerenter'); }
+        // try { element.onpointermove = null; } catch (error) { } finally { allowEvent(element, 'pointermove'); }
+        // try { element.onpointerover = null; } catch (error) { } finally { allowEvent(element, 'pointerover'); }
+        try { element.onpointerout = null; } catch (error) { } finally { allowEvent(element, 'pointerout'); }
+        try { element.onpointerleave = null; } catch (error) { } finally { allowEvent(element, 'pointerleave'); }
+        try { element.onpointercancel = null; } catch (error) { } finally { allowEvent(element, 'pointercancel'); }
+        // 取消通过 JavaScript 实现的焦点离开检测
+        try { element.onfocus = null; } catch (error) { } finally { allowEvent(element, 'focus'); }
+        try { element.onfocusin = null; } catch (error) { } finally { allowEvent(element, 'focusin'); }
+        try { element.onfocusout = null; } catch (error) { } finally { allowEvent(element, 'focusout'); }
+        try { element.onblur = null; } catch (error) { } finally { allowEvent(element, 'blur'); }
+        // 防止 JavaScript 事件检测
+        preventEventChecks(element);
+    };
+    let allowElementRecursion = function (element) {
+        allowElement(element);
+        for (let i = 0; i < element.children.length; i++) {
+            allowElementRecursion(element.children.item(i));
+        }
+    };
     // let removeAllListeners = function (old_element) {
     //     let new_element = old_element.cloneNode(true);
     //     old_element.parentNode.replaceChild(new_element, old_element);
     // };
     let removeHiddenElements = function (element, recursion) {
-        if (element.style.display == 'none' || element.style.visibility == 'hidden') {
+        if ((element.hidden || element.style.display == 'none' || element.style.visibility == 'hidden' || ((element.style.height <= 0 || element.style.width <= 0) && element.style.overflow == 'hidden') && element.children.length == 0)) {
             console.info(`Remove <${element.tagName} id='${element.id}' class='${element.className}' />`);
             element.remove();
         } else if (recursion) {
@@ -137,6 +234,12 @@
             if (main_container_element === null) { // 检查 id='main' 的标签
                 main_container_element = document.getElementById('main');
             }
+            if (main_container_element === null) { // 检查 id='main-content' 的标签
+                main_container_element = document.getElementById('main-content');
+            }
+            if (main_container_element === null) { // 检查 id='main-contents' 的标签
+                main_container_element = document.getElementById('main-content');
+            }
             if (main_container_element === null) { // 检查 id='content' 的标签
                 main_container_element = document.getElementById('content');
             }
@@ -145,6 +248,14 @@
             }
             if (main_container_element === null) { // 检查 class='main' 的标签
                 let elements = document.getElementsByClassName('main');
+                if (elements.length > 0) { main_container_element = elements[0]; }
+            }
+            if (main_container_element === null) { // 检查 class='main-content' 的标签
+                let elements = document.getElementsByClassName('content');
+                if (elements.length > 0) { main_container_element = elements[0]; }
+            }
+            if (main_container_element === null) { // 检查 class='main-contents' 的标签
+                let elements = document.getElementsByClassName('content');
                 if (elements.length > 0) { main_container_element = elements[0]; }
             }
             if (main_container_element === null) { // 检查 class='content' 的标签
@@ -160,35 +271,35 @@
     };
 
     // 对抗延迟运行（即在此脚本执行后运行）的禁用程序和循环执行的禁用程序，
-    setInterval( // 每 0.2 秒执行一次。
+    executeWithInterval( // 每 0.2 秒执行一次。
         (function () {
             if (document) {
                 try { allowElement(document); } catch (error) { } finally { }
             }
         }), 200
     );
-    setInterval( // 每 0.3 秒执行一次。
+    executeWithInterval( // 每 0.3 秒执行一次。
         (function () {
             if (document) {
                 try { allowElement(document.body); } catch (error) { } finally { }
             }
         }), 300
     );
-    setInterval( // 每 0.5 秒执行一次。
+    executeWithInterval( // 每 2 秒执行一次。
         (function () {
             let mce = getMainContainerElement()
             if (document && mce) {
-                try { allowElement(mce); } catch (error) { } finally { }
+                try { allowElementRecursion(mce); } catch (error) { } finally { }
             }
-        }), 500
+        }), 2000
     );
 
     // 对抗延迟运行（即在此脚本执行后运行）的混淆程序和循环执行的混淆程序，
     setTimeout(
-        // 延迟 0.6 秒，有助于动态加载的内容的显示。
+        // 延迟 1 秒，有助于动态加载的内容的显示。
         function () {
             console.debug('Start scanning for hidden elements');
-            setInterval(
+            executeWithInterval(
                 // 每 2 秒执行一次。
                 function () {
                     if (document) {
@@ -203,7 +314,7 @@
                     }
                 }, 2000
             );
-        }, 600
+        }, 1000
     )
 
     console.debug('Complete the installation of user-script/remove-limits');
